0358be51f155773d1099f5fc1c65f0f323648c3c
Hemant Sah <hemantsah2912@gmail.com>
2024-09-26 21:19:57 +0530

    book store done

---
+++ Frontend/src/components/Course.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import Cards from "./Cards";
+import axios from "axios";
+import { Link } from "react-router-dom";
+function Course() {
+  const [book, setBook] = useState([]);
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("http://localhost:4001/book");
+        console.log(res.data);
+        setBook(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+  return (
+    <>
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="mt-28 items-center justify-center text-center">
+          <h1 className="text-2xl  md:text-4xl">
+            We're delighted to have you{" "}
+            <span className="text-pink-500"> Here! :)</span>
+          </h1>
+          <p className="mt-12">
+          Books are windows into different worlds, offering knowledge, entertainment, and inspiration. They capture the essence of human experience, preserving history, culture, and imagination. From fiction to non-fiction, books cater to diverse interests, allowing readers to explore new ideas, gain insights, and develop critical thinking. They have the power to educate, challenge, and transform perspectives, making them essential tools for personal and intellectual growth. In an increasingly digital age, books remain timeless treasures, providing solace, adventure, and wisdom. Whether in print or digital form, they continue to connect us to the past, inform our present, and inspire our future.
+          </p>
+          <Link to="/">
+            <button className="mt-6 bg-pink-500 text-white px-4 py-2 rounded-md hover:bg-pink-700 duration-300">
+              Back
+            </button>
+          </Link>
+        </div>
+        <div className="mt-12 grid grid-cols-1 md:grid-cols-4">
+          {book.map((item) => (
+            <Cards key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Course;
